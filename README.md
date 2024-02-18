6439e2e91132af109da92135a061ce9a19da6490
tomyferra <ferra.tomy@gmail.com>
2024-02-18 10:30:19 -0300

    Improved work experience and readme

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 https://tomyferra.github.io/Portfolio/
 
 #
-Welcome to my portfolio! I'm a backend developer with a strong desire to become a full-stack developer. With 4 years of experience in backend development and 2 years in front-end development, I possess a wide range of skills and expertise.
+Welcome to my portfolio! I'm a backend developer with a strong desire to become a full-stack developer. With 5 years of experience in backend development and 3 years in front-end development, I possess a wide range of skills and expertise.
 
-My backend skills include Python and SQL. I have extensive experience in building scalable, robust, and efficient backend systems using Python. Additionally, my proficiency in SQL allows me to create and manage complex databases with ease.
+My backend skills include Python, Django, Flask and SQL. I have extensive experience in building scalable, robust, and efficient backend systems using Python. Additionally, my proficiency in SQL allows me to create and manage complex databases with ease.
 
-On the front-end, I have expertise in React, Bootstrap, and Javascript. I have developed several web applications using these technologies, with a strong focus on user experience and functionality.
+On the front-end, I have expertise in React, Git, Bootstrap, and Javascript. I have developed several web applications using these technologies, with a strong focus on user experience and functionality.
 
 I'm located in Buenos Aires, Argentina, and I'm always looking for new and exciting opportunities to expand my skill set and further my career. If you're interested in working together or want to learn more about my experience and skills, please feel free to contact me.
 #
 ## Backend Development
-My backend skills include Python and SQL. I have extensive experience in building scalable, robust, and efficient backend systems using Python. Additionally, my proficiency in SQL allows me to create and manage complex databases with ease.
+My backend skills include Python and SQL. I have extensive experience in building scalable, robust, and efficient backend systems using Python, Django and Flask. Additionally, my proficiency in SQL allows me to create and manage complex databases with ease.
 
 ## Frontend Development
 On the front-end, I have expertise in React, Bootstrap, and Javascript. I have developed several web applications using these technologies, with a strong focus on user experience and functionality.
@@ -24,6 +24,6 @@ I'm located in Buenos Aires, Argentina, and I'm always looking for new and excit
 ## Contact details
 * Mail: ferra.tomy@gmail.com
 * Phone: +54 9 (11) 69002457
-* Portfolio: https://tomyferra.github.io/Portfolio/
+* Portfolio: https://tomasferrariportfolio.onrender.com
 * Linkedin: https://www.linkedin.com/in/tomasmariaferrari/
 * Github: https://github.com/tomyferra
--- src/components/WorkExperienceData.js
+++ src/components/WorkExperienceData.js
@@ -4,28 +4,28 @@ export const WorkExperienceData = [
   {
     jobname: 'Software Engineer',
     companyname: 'JP Morgan Chase',
-    description: "Python backend Software Engineer. Scripts developer in Python for financial internal tools. Object Oriented Programming, Unit Testing, Code reviews, Agile Methodologies. Certified AWS Cloud Practitioner",
+    description: "• Developed and implemented automated scripts in Python using OOP to streamline financial reporting processes resulting in a reduction of financial incidents. • Led and successfully orchestrated the migration from RedisCache to the AMPS server, ensuring a seamless transition and optimized performance. Achieved a notable improvement in throughput by 25% and a reduction in incidents. Coded all necessary scripts and conducted thorough unit tests to guarantee system reliability and stability.  •	Got certified as AWS Cloud Practitioner. •	As Scrum Master, I improved agile methodologies and scrum practices, resulting in a 40% increase in team productivity and throughput of deliverables. ",
     datestarted: 'Dec 2021',
     dateended: 'Actuality',
-    technologies: 'HTML, CSS, Scrum, Agile, UnitTests, OOP, GIT, Python, SQL, React, AWS',
+    technologies: 'Python, OOP, SQL, GIT, AWS, React, Javascript, Unit Testing, Agile Methodologies, Splunk, PowerBi, Tableau',
     id: uuidv4()
   },
   {
     jobname: 'Developer',
     companyname: 'Exxon Mobil',
-    description: "Dashboard development used as reporting and monitoring tools. Infrastructure, network and UX monitoring through Splunk, AppDynamics, Tableau and PowerBI. Machine Learning services offered by Splunk. Agile methodology under Scrum framework.",
+    description: "•	Infrastructure, network and APM monitoring through Splunk, Appdynamics, Tableau and PowerBi. Automatic triggering actions to take effect if threshold are achieved. • Developed an AI powered chatbot using NLP specifically designed to support incident managers in rapidly retrieving crucial information during ongoing incidents reducing Mean Time To Restore (MTTR) incidents.  •	Developed Python – Flask backend API’s and React Frontend for web applications, ensuring seamless user experience and optimized performance simplifying the analysis of common root causes and enhancing the decision-making process.",
     datestarted: 'Dec 2019',
     dateended: 'Dec 2021',
-    technologies: 'HTML, CSS, Scrum, Agile, UnitTests, OOP, GIT, Python, SQL,Splunk, PowerBi, Tableau',
+    technologies: 'Python, Django, Flask, Splunk, PowerBi, Tableau, AppDynamics, React, Git, SQL, Azure, Agile, Unit Testing, OOP',
     id: uuidv4()
   },
   {
     jobname: 'RPA Bot Developer',
     companyname: 'Everis',
-    description: "Robotic Process Automation using Automation Anywhere. Develop end to end project plans with bots that emulate human behavior. Provide development and testing support.",
+    description: "•	Establishing, testing, and overseeing automated workflows to guarantee optimal efficiency in business processes while mitigating the risk of errors. • Got certified as a professional RPA Bot developer using Automation Anywhere tool. ",
     datestarted: 'June 2019',
     dateended: 'Dec 2019',
-    technologies: ["App Dynamics"],
+    technologies: ["Automation Anywhere, RPA, C#, Infrastructure, Agile"],
     id: uuidv4()
   }
   ];
